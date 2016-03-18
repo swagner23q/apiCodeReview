83aa6b8ea7f84997d62657ee8846b108939ffbe1
sara <sara.wagner23q@gmail.com>
2016-03-18 13:43:24 -0700

    add mass styling

--- js/github-interface.js
+++ js/github-interface.js
@@ -6,12 +6,11 @@ $(document).ready(function() {
    $('#buttonSubmit').click(function() {
      var inputtedUser = $("#gitUserName").val();
 
-
+     $(".results").show();
      $(".results").empty();
      $.get('https://api.github.com/users/' + inputtedUser + '/repos' + '?access_token=' + apiKey).then(function(response){
        for(var i = 0; i < response.length; i++) {
-        $(".results").append("<li> Repo Name: " + response[i].name + "<br>" + "Date Created: " + moment(response[i].created_at).format("LLL") + "<br><br>" + "Description: " + response[i].description + "</li><p></p>");
-
+        $(".results").append("<li><span class='key'> Repo Name: </span>" + "<span class='title'>" + response[i].name + "</span><br>" + "<span class='key'>Date Created: </span>" + moment(response[i].created_at).format("LLL") + "<br>" + "<span class='key'>Description: </span>" + response[i].description + "</li><br>");
       }
     });
       $.get('https://api.github.com/users/' + inputtedUser + '?access_token=' + apiKey).then(function(response){
@@ -22,4 +21,3 @@ $(document).ready(function() {
    });
   });
 });
-// "Date Created: " + moment(response[i].created_at).format("LTS") + "<br><br>" +
