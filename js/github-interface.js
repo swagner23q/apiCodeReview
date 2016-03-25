721d6d6cf13c5ed534a8b097cb1aba25bf1e6299
sara <sara.wagner23q@gmail.com>
2016-03-24 20:25:07 -0700

    separated files with errors

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,23 +1,13 @@
 //User Interface Logic
-
 var apiKey = require('../.env').apiKey;
+var getRepos = require('./github.js').getRepos;
 
 $(document).ready(function() {
-   $('#buttonSubmit').click(function() {
-     var inputtedUser = $("#gitUserName").val();
-     $(".results").show();
-     $(".results").empty();
+  $('#buttonSubmit').click(function() {
+   var inputtedUser = $("#gitUserName").val();
+   $(".results").show();
+   $(".results").empty();
+   getRepos();
 
-     $.get('https://api.github.com/users/' + inputtedUser + '/repos' + '?access_token=' + apiKey).then(function(response){
-       for(var i = 0; i < response.length; i++) {
-        $(".results").append("<li><span class='key'> Repo Name: </span>" + "<span class='title'>" + response[i].name + "</span><br>" + "<span class='key'>Date Created: </span>" + moment(response[i].created_at).format("LLL") + "<br>" + "<span class='key'>Description: </span>" + response[i].description + "</li><br>");
-      }
-    });
-      $.get('https://api.github.com/users/' + inputtedUser + '?access_token=' + apiKey).then(function(response){
-         $(".userProfiles").html("<img src=' " + response.avatar_url + " '>" + "<br><br>Name: " + response.name + "<br><br>Location: " + response.location + "<br><br>Followers: " + response.followers + "<br><br>Total Repositories: " + response.public_repos);
-
-     }).fail(function(error){
-       console.log(error.responseJSON.message);
-   });
   });
 });
---
+++ js/github.js
@@ -0,0 +1,13 @@
+//Business Logic
+exports.getRepos = function() {
+  $.get('https://api.github.com/users/' + inputtedUser + '/repos' + '?access_token=' + apiKey).then(function(response){
+    for (var i = 0; i < response.length; i++) {
+     $(".results").append("<li><span class='key'> Repo Name: </span>" + "<span class='title'>" + response[i].name + "</span><br>" + "<span class='key'>Date Created: </span>" + moment(response[i].created_at).format("LLL") + "<br>" + "<span class='key'>Description: </span>" + response[i].description + "</li><br>");
+   }
+ });
+    $.get('https://api.github.com/users/' + inputtedUser + '?access_token=' + apiKey).then(function(response){
+      $(".userProfiles").html("<img src=' " + response.avatar_url + " '>" + "<br><br>Name: " + response.name + "<br><br>Location: " + response.location + "<br><br>Followers: " + response.followers + "<br><br>Total Repositories: " + response.public_repos);
+      }).fail(function(error){
+        console.log(error.responseJSON.message);
+      });
+};
